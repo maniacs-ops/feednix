e35f7ec3d8cfc058ceb88dd027dbd7af79850cce
Jorge Martinez Hernandez <jorgemartinezhernandez@gmail.com>
2015-03-11 08:45:20 -0500

    Fixed a few errors

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,2 @@
 autoreconf -i
-diff -Naur $HOME/.config/feednix/config.json ./config.json > patchfile
-patch $HOME/.config/feednix/config.json < patchfile
 sudo mkdir -p /etc/xdg/feednix && sudo cp config.json /etc/xdg/feednix
