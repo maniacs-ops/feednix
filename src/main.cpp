8fee2f93913008c1a409bb06ce0b4c7d96afe030
Jorge Martinez Hernandez <jorge.martinezhernandez@protonamil.ch>
2015-06-19 14:29:41 -0500

    Avoided abort on exit

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@ void atExitFunction(void){
     system(std::string("rm -R " + TMPDIR + " 2> /dev/null").c_str());
     if(curses != NULL)
         curses->cleanup();
-    std::cout << "Aborted.\nCheck log for details." << std::endl;
 }
 
 void sighandler(int signum){
